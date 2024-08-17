6b771969b8e1c446c65fdd6333293b774cb2e393
JSTM4NU <110738432+JSTM4NU@users.noreply.github.com>
2024-08-17 02:43:55 +0200

    Add files via upload

--- config/config.lua
+++ config/config.lua
@@ -1,5 +1,5 @@
 Config = {}
 Config.DefaultNumberOfCharacters = 1
 Config.PlayersNumberOfCharacters = {
-    { license2 = "license2:4e6ae2096783fe0fe2ca94dbc10acccb62b3a195", allowedCharacters = 4 },
+    { license2 = "license2:4e6ae2096783fe0fe2ca94dbc10acccb62b3a195", allowedAmount = 4 },
 }
